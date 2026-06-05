eea57af8d84b8a93cd7289b284ddf71e115ca579
Александа Синицына <alexsandra217@mail.ru>
2026-06-05 20:05:10 +0500

    add eslint.config.js and update

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import getResult from "./src/index.js";
-console.log(getResult());
+import getResult from './src/index.js'
+console.log(getResult())
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import _ from "lodash";
+import _ from 'lodash'
 
 export default () => {
-  return _.last(["one", "two"]);
-};
+  return _.last(['one', 'two'])
+}
